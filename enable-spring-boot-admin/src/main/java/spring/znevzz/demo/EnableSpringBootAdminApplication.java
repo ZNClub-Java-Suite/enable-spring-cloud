efb67b413aa7c223b38db83f091d90073a3da5fa
ZNevzz <dsouza.nevil45@gmail.com>
2019-08-24 11:23:23 +0530

    Admin-Discovery

--- enable-spring-boot-admin/src/main/java/spring/znevzz/demo/EnableSpringBootAdminApplication.java
+++ enable-spring-boot-admin/src/main/java/spring/znevzz/demo/EnableSpringBootAdminApplication.java
@@ -1,9 +1,11 @@
 package spring.znevzz.demo;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class EnableSpringBootAdminApplication {
 
 	public static void main(String[] args) {
