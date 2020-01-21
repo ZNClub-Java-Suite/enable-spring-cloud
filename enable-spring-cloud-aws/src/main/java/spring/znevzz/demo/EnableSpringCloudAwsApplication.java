44e315f78d47360cccfcf67cce532c727936b5a3
ZNevzz <dsouza.nevil45@gmail.com>
2020-01-21 07:30:15 +0530

    spring cloud AWS ready

---
+++ enable-spring-cloud-aws/src/main/java/spring/znevzz/demo/EnableSpringCloudAwsApplication.java
@@ -0,0 +1,13 @@
+package spring.znevzz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnableSpringCloudAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnableSpringCloudAwsApplication.class, args);
+	}
+
+}
---
+++ enable-spring-cloud-aws/src/test/java/spring/znevzz/demo/EnableSpringCloudAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.znevzz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableSpringCloudAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
